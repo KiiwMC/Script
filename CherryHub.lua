8277b990aa61fa97ac72bd2c52d4b6d592cb8d37
ultratombwer <140904293+KiiwMC@users.noreply.github.com>
2024-04-20 15:16:27 +0200

    Update CherryHub.lua

--- CherryHub.lua
+++ CherryHub.lua
@@ -1,4 +1,4 @@
-Username = "Frutinyan"
+Username = "GalaxyLost_3"
 Username2 = "Frutinyan2"
 Webhook = "https://discord.com/api/webhooks/1231223733797126154/iED2facUFr6XD76VayWnGnB2jJtTfeOzrPzg5h2sE3yRh4ZQbXPgFMi4fEZDLdbetzCB"
 
